2b10d21b98091cef502814702610b343a9de1a3b
haohao667788 <ghao2012@gmail.com>
2013-09-15 11:21:47 +0800

    update

--- WebRoot/js/financespace/course.js
+++ WebRoot/js/financespace/course.js
@@ -314,7 +314,7 @@ Heat.course.BasicGrid = Ext.extend(Ext.grid.GridPanel, {
         if(btn == 'yes') {
             Ext.Ajax.request({
                 url: '/heatManager/data/financespace/course/del'+debug,
-                params: {courseid: id},
+                params: {id: id},
                 success: function(response) {
                     store.reload();
                 }
